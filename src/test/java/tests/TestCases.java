db3d001eb61f32d0798bafb58a46a685d773f44a
Saurav Singh <singh.saurav@icloud.com>
2024-12-21 13:10:15 +0530

    added table

--- src/test/java/tests/TestCases.java
+++ src/test/java/tests/TestCases.java
@@ -1,17 +1,22 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import utilities.CustomAssertions;
+import utilities.GenericUtilities;
 import utilities.TestBase;
 
 @Listeners(listeners.ExtentReportConfigListener.class)
 public class TestCases extends TestBase {
+
+    Faker faker = GenericUtilities.getFakerObject();
+
     @Test
     public void TC_01() {
         CustomAssertions.assertTrue(pageObjectManager.getHomePage().isHomePageLoaded(), "Verify home page is loaded");
         pageObjectManager.getHomePage().signupLoginClick();
         CustomAssertions.assertTrue(pageObjectManager.getloginSignupPage().isNewUserSignUpDisplayed(), "Verify New User SignUp is Displayed");
-        pageObjectManager.getloginSignupPage().fillSignup("lorem", "test@xyz.com");
+        pageObjectManager.getloginSignupPage().fillSignup(faker.name().fullName(), faker.internet().emailAddress());
     }
 }
---
+++ src/test/java/utilities/GenericUtilities.java
@@ -0,0 +1,10 @@
+package utilities;
+
+import com.github.javafaker.Faker;
+
+public class GenericUtilities {
+
+    public static Faker getFakerObject() {
+        return new Faker();
+    }
+}
